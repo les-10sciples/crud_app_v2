818e503dc76ffa31bb0780ad180e44f7b3164ef4
Titouan GRAGNIC <titouan.gragnic@epita.fr>
2025-10-20 17:02:55 +0200

    fix port

--- backend/backend.py
+++ backend/backend.py
@@ -47,4 +47,4 @@ def update_task():
     return jsonify({"tasks": db.GetTodolist()}), 201
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
